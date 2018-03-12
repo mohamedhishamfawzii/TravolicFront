67b923401de3bd85a0ed4d2c669106660f5caa22
hisham <m.hisham@live.com>
2018-03-12 18:55:46 +0200

    edits

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  mobVersion;
+  webVersion = true;
+  width = screen.width;
   constructor() { }
 
   ngOnInit() {
+    if (this.width < 740 ) {
+      this.mobVersion = true;
+      this.webVersion = false;
+    }
   }
 
 }
