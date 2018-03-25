c232f2f3f6b5a44b1c4074b6bcc0fe78108af301
YousefMansy <y.o.mansy@gmail.com>
2018-03-26 01:16:43 +0200

    Updated Mobile Version as per Sprint 1 Modifications

--- src/app/searchdiv/searchdiv.component.ts
+++ src/app/searchdiv/searchdiv.component.ts
@@ -9,17 +9,21 @@ import {BackendService} from '../backend.service';
 })
 export class SearchdivComponent implements OnInit {
 type = 'oneway';
+adults = ['1 adult', '2 adults', '3 adults', '4 adults', '5 adults', '6 adults', '7 adults', '8 adults', '9 adults'];
+children = ['1 child', '2 children', '3 children', '4 children', '5 children', '6 children', '7 children', '8 children', '9 children'];
+infants = ['1 infant', '2 infants', '3 infants', '4 infants', '5 infants', '6 infants', '7 infants', '8 infants', '9 infants'];
+
 airports;
 location;
 isSpinningFrom = false;
-isSpinningTo =false;
+isSpinningTo = false;
   constructor(private backendConnect: BackendService, private http: Http) { }
   mobVersion;
   webVersion = true;
   width = screen.width;
   from ;
   to;
-  Aairports=[];
+  Aairports = [];
   i;
   ngOnInit() {
     if (this.width < 860 ) {
@@ -30,9 +34,9 @@ isSpinningTo =false;
       .subscribe((res) => {
          this.airports = res.json();
          this.backendConnect.getLocation().then((result)=> {
-           this.location=result.json();
+           this.location = result.json();
           console.log("location result",this.location);
-           for ( var i =0 ; i<1000;i++ ){
+           for (var i = 0; i < 1000; i++){
              this.Aairports.push(this.airports[i]);
            }
            this.Aairports.forEach( (air) =>{
